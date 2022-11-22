98c325c5d6e7bb4c37f5b035855115570832065c
Sebastian <ippo_sebas@hotmail.com>
2022-11-22 09:30:08 -0500

    Codigo postal problem fixed

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ import pandas as pd
 import time
 
 encoding: utf_8
-filesheet = pd.read_excel('100_datos_prueba.xlsx', usecols='E:H')
+filesheet = pd.read_excel('Data_Prueba.xlsx', usecols='E:H')
 
-cp = list(filesheet.loc[:, 'CP'].zfill(2))
+cp = list(filesheet.loc[:, 'CP'])
 dir = list(filesheet.loc[:, 'DIRECCION'])
 loc = list(filesheet.loc[:, 'LOCALIDAD'])
-print(cp)
+cp_valor_final = []
+
+for i in range(len(cp)):
+    cp_valor_final.append(str(cp[i]).zfill(5))
 
 valor_cup = ""
 valor_potenciaP1 = ""
@@ -20,13 +23,12 @@ valor_potenciaP1 = ""
 datas = {"cups": [],
          "potencia": []}
 
-
 class webScrapping():
     def recolectar_cups_potenciaP1(self):
-        for i in range(20):
+        for i in range(len(cp)):
             driver.find_element(By.CSS_SELECTOR, "#address").click()
             time.sleep(2)
-            driver.find_element(By.CSS_SELECTOR, "#postalCode").send_keys(cp[i])
+            driver.find_element(By.CSS_SELECTOR, "#postalCode").send_keys(cp_valor_final[i])
             time.sleep(2)
             city = driver.find_element(By.CSS_SELECTOR, "#city")
             selector_city = Select(city)
@@ -47,9 +49,6 @@ class webScrapping():
             datas["cups"].append(valor_cup)
             valor_potenciaP1 = potenciaP1.text[24:len(potenciaP1.text)]
             datas["potencia"].append(valor_potenciaP1)
-
-            print(valor_cup)
-            print(valor_potenciaP1)
             driver.refresh()
 
 
@@ -59,7 +58,7 @@ driver.maximize_window()
 driver.get(url)
 trabajo = webScrapping()
 trabajo.recolectar_cups_potenciaP1()
-print(datas)
+
 df = pd.DataFrame(datas)
 with pd.ExcelWriter('100_datos_prueba.xlsx', mode='a') as writer:
     df.to_excel(writer, sheet_name='Hoja2')
